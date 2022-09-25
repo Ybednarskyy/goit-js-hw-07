25be5835fa3eae29fd43eef56f9072968c1c0913
Ybednarskyy <y.bednarskyy@gmail.com>
2022-09-26 00:36:39 +0300

    Finished 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galaryBoxRef = document.querySelector(".gallery");
+const markup = createImagesListMarkup(galleryItems);
+galaryBoxRef.insertAdjacentHTML("beforeend", markup);
+galaryBoxRef.addEventListener("click", onImageClick);
 
-console.log(galleryItems);
+function createImagesListMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+function onImageClick(evt) {
+  console.log(evt.target);
+  blockStandartAction(evt);
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}"></img>`
+  );
+  instance.show();
+  //   console.log(instance.visible());
+
+  galaryBoxRef.addEventListener("keydown", (evt) => {
+    if (instance.visible && evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
