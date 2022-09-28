04d5e350dd68315cfda148edfd045236111ead24
Ybednarskyy <y.bednarskyy@gmail.com>
2022-09-28 22:16:07 +0300

    Finshed task 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -3,48 +3,20 @@ import { galleryItems } from "./gallery-items.js";
 const galaryBoxRef = document.querySelector(".gallery");
 const markup = createImagesListMarkup(galleryItems);
 galaryBoxRef.insertAdjacentHTML("beforeend", markup);
-galaryBoxRef.addEventListener("click", onImageClick);
 
 function createImagesListMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`;
+      return `<li class="gallery__item">
+      <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>
+    </li>`;
     })
     .join("");
 }
 
-function onImageClick(evt) {
-  console.log(evt.target);
-  blockStandartAction(evt);
-
-  if (evt.target.nodeName !== "IMG") {
-    return;
-  }
-
-  const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}"></img>`
-  );
-  instance.show();
-  //   console.log(instance.visible());
-
-  galaryBoxRef.addEventListener("keydown", (evt) => {
-    if (instance.visible && evt.code === "Escape") {
-      instance.close();
-    }
-  });
-}
-
-function blockStandartAction(evt) {
-  evt.preventDefault();
-}
-
-console.log(galleryItems);
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
